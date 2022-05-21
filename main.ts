ec194396b1a28c4e76aa0e3d89a8a572892ac0a9
Hong Minhee <hong.minhee@gmail.com>
2022-05-22 04:14:33 +0900

    Use cliffy instead of std/flags for CLI parsing

--- main.ts
+++ main.ts
@@ -1,43 +1,51 @@
 import { handlers, setup } from "https://deno.land/std@0.140.0/log/mod.ts";
-import { parse } from "https://deno.land/std@0.140.0/flags/mod.ts";
+import {
+  Command,
+  ITypeInfo,
+  ValidationError,
+} from "https://deno.land/x/cliffy@v0.24.2/command/mod.ts";
 import { getRecentChanges } from "./wiki.ts";
 
-await setup({
-  handlers: {
-    console: new handlers.ConsoleHandler("DEBUG"),
-  },
-  loggers: {
-    wiki: { level: "INFO", handlers: ["console"] },
-  },
-});
-
-const options = parse(Deno.args, {
-  unknown(opt: string) {
-    if (!opt.startsWith("-")) return opt;
-    console.error(`error: unknown option ${opt}`);
-    Deno.exit(1);
-  },
-});
-
-if (options._.length < 1) {
-  console.error("error: no wiki url specified");
-  console.error("usage: cli.ts [OPTIONS] WIKI_URL");
-  Deno.exit(1);
+function urlType({ label, name, value }: ITypeInfo): URL {
+  try {
+    return new URL(value);
+  } catch (_) {
+    throw new ValidationError(
+      `${label} "${name}" must be a valid url, ` +
+        `but got "${value}"`,
+    );
+  }
 }
 
-let wikiUrl;
-try {
-  wikiUrl = new URL(options._[0].toString());
-} catch (_) {
-  console.error(`error: not a valid url: ${options._[0]}`);
-  Deno.exit(1);
+async function main() {
+  await setup({
+    handlers: {
+      console: new handlers.ConsoleHandler("DEBUG"),
+    },
+    loggers: {
+      wiki: { level: "INFO", handlers: ["console"] },
+    },
+  });
+
+  await new Command()
+    .name(import.meta.url.match(/\/([^/]+)$/)?.[1]!)
+    .description(
+      "Relay RecentChanges from a MediaWiki site to a Mastodon account",
+    )
+    .type("url", urlType)
+    .arguments("<wiki_url:url>")
+    .allowEmpty(false)
+    .action(async (options, wikiUrl) => {
+      const rcOptions = {
+        window: 10,
+        namespace: 0,
+      };
+
+      for await (const rc of getRecentChanges(wikiUrl, rcOptions)) {
+        console.log(rc);
+      }
+    })
+    .parse(Deno.args);
 }
 
-const rcOptions = {
-  window: 10,
-  namespace: 0,
-};
-
-for await (const rc of getRecentChanges(wikiUrl, rcOptions)) {
-  console.log(rc);
-}
+if (import.meta.main) await main();
